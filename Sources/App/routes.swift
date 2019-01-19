d195bff0485244d1d6c4adef65428ed63b89b551
Igor Chertenkov <igor.chertenkov@sibers.com>
2019-01-19 17:22:44 +0700

    reversed mistakenly deleted route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -17,5 +17,8 @@ public func routes(_ router: Router) throws {
     router.get("maps", "json", Int.parameter, use: gameMapController.json)
     router.get("maps", "random", use: gameMapController.random)
     router.get("maps", "seed", String.parameter, use: gameMapController.withSeed)
+
+    router.post("maps", use: gameMapController.create)
+
     //router.delete("maps", GameMap.parameter, use: gameMapController.delete)
 }
